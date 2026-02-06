5b0a3a28c398bb15683050212e9f8fad0568a7fc
Ares <ariskoliadimas@gmail.com>
2026-02-07 01:23:48 +0200

    Replace hardcoded colors with theme-specific CSS variables in styles and constants. Clean up IntelliJ configuration file.

--- src/app/constants/constants.ts
+++ src/app/constants/constants.ts
@@ -1,5 +1,4 @@
 export const START_YEAR: number = 2026;
 export const CURRENT_YEAR: number = new Date().getFullYear();
-export const ONE_DOT: [string] = ["limegreen"];
+export const ONE_DOT: [string] = ['var(--tui-background-accent-1)'];
 export const ALLOWANCE: number = 22;
-
